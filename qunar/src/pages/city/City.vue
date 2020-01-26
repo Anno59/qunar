cdd11818473b03ff5cca328037715129e249afb3
Anno59 <372025284@qq.com>
2020-01-26 11:31:57 +0800

    add style and structure on list of search

--- qunar/src/pages/city/City.vue
+++ qunar/src/pages/city/City.vue
@@ -2,17 +2,49 @@
 	<div>
 		<city-header></city-header>
 		<city-search></city-search>
+		<city-list
+				:hotCities="hotCities"
+				:cities="cities">
+		</city-list>
+		<city-alphabet :cities="cities"></city-alphabet>
 	</div>
 </template>
 
 <script>
   import CityHeader from './components/Header'
   import CitySearch from './components/Search'
+  import CityList from './components/List'
+  import CityAlphabet from './components/Alphabet'
+	import axios from 'axios'
   export default {
     name: 'City',
 		components: {
       CityHeader,
-      CitySearch
+      CityList,
+      CitySearch,
+      CityAlphabet
+		},
+		data(){
+      return {
+        hotCities: [],
+        cities: {},
+			}
+		},
+		methods: {
+      getCityInfo(){
+        axios.get('/api/city.json')
+				.then(this.getCityInfoSucc)
+			},
+      getCityInfoSucc(res){
+        res = res.data;
+        if(res && res.ret){
+          this.hotCities = res.data.hotCities;
+          this.cities = res.data.cities;
+				}
+			}
+		},
+		mounted() {
+			this.getCityInfo();
 		}
   }
 </script>
