dd67f2152c1223441b57544547a02d7fdd47c381
Anno59 <372025284@qq.com>
2020-01-26 19:56:55 +0800

    add logic of list

--- qunar/src/pages/city/components/Search.vue
+++ qunar/src/pages/city/components/Search.vue
@@ -1,14 +1,77 @@
 <template>
 	<div>
 		<div class="search">
-			<input class="search-input" type="text" placeholder="输入城市名或拼音" />
+			<input v-model="keywords" class="search-input" type="text" placeholder="输入城市名或拼音" />
+		</div>
+		<div
+				class="search-content"
+				ref="search"
+		>
+			<ul>
+				<li
+						class="search-item border-bottom"
+						v-for="(city, index) in item"
+						:key="index"
+				>
+					{{city}}
+				</li>
+				<li class="search-item border-bottom"
+						v-show="hasItem"
+				>
+					没有找到匹配数据
+				</li>
+			</ul>
 		</div>
 	</div>
 </template>
 
 <script>
+	import Bscroll from 'better-scroll'
   export default {
-    name: 'CitySearch'
+    name: 'CitySearch',
+		props: {
+    	cities: Object,
+		},
+		data(){
+      return {
+        keywords: '',
+				item: [],
+				timer: null,
+			}
+		},
+		computed: {
+      hasItem() {
+        return !this.item.length;
+			}
+		},
+		watch: {
+    	keywords(){
+    	  let timer = this.timer;
+    	  if(timer){
+    	    clearTimeout(timer);
+				}
+    	  if(!this.keywords){
+    	    this.item = []
+					return
+				}
+				timer = setTimeout(() => {
+					const keywords = this.keywords;
+					const item = [];
+					for(let i in this.cities){
+						this.cities[i].forEach(function(e){
+							if(e.spell.indexOf(keywords) > -1
+								|| e.name.indexOf(keywords) > -1){
+								item.push(e.name)
+							}
+						})
+					}
+					this.item = item;
+				}, 100)
+			}
+		},
+		mounted(){
+      this.scroll = new Bscroll(this.$refs.search)
+		}
   }
 </script>
 
