f886d97878ecee3f862201515b495e01fb18ca2a
Anno59 <372025284@qq.com>
2020-01-28 17:25:04 +0800

    add recurring components

--- qunar/src/pages/detail/Detail.vue
+++ qunar/src/pages/detail/Detail.vue
@@ -3,9 +3,9 @@
 		<detail-banner
 				:bannerImg="bannerImg"></detail-banner>
 		<detail-header></detail-header>
-<!--		<div class="content">-->
-<!--			<detail-list></detail-list>-->
-<!--		</div>-->
+		<div class="content">
+			<detail-list :categoryList="list"></detail-list>
+		</div>
 		<div class="content"></div>
 	</div>
 </template>
@@ -13,16 +13,33 @@
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
+import axios from 'axios'
+
 export default {
 	name: 'Detail',
 	components: {
 		DetailBanner,
 		DetailHeader,
+		DetailList
 	},
 	data(){
 		return {
+		  list: [],
 			bannerImg: 'http://img1.qunarzz.com/sight/p0/201301/16/aecd792aa219fd0793835fbb.jpg_600x330_8a61adc9.jpg'
 		}
+	},
+	methods: {
+		async getDetailInfo() {
+		  const detail = await axios.get('/api/detail.json');
+		  const resData = detail.data;
+		  if(resData && resData.ret){
+		    this.list = resData.data.categoryList
+			}
+		}
+	},
+	mounted() {
+	  this.getDetailInfo ()
 	}
 }
 </script>
