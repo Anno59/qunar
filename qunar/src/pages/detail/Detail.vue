4d87121b0f95dfcd2132c8a741527bea6aec11c4
Anno59 <372025284@qq.com>
2020-01-28 16:51:46 +0800

    add header

--- qunar/src/pages/detail/Detail.vue
+++ qunar/src/pages/detail/Detail.vue
@@ -2,19 +2,22 @@
 	<div>
 		<detail-banner
 				:bannerImg="bannerImg"></detail-banner>
-<!--		<detail-header></detail-header>-->
+		<detail-header></detail-header>
 <!--		<div class="content">-->
 <!--			<detail-list></detail-list>-->
 <!--		</div>-->
+		<div class="content"></div>
 	</div>
 </template>
 
 <script>
-import DetailBanner from './components/banner'
+import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
 export default {
 	name: 'Detail',
 	components: {
 		DetailBanner,
+		DetailHeader,
 	},
 	data(){
 		return {
---
+++ qunar/src/pages/detail/components/Header.vue
@@ -0,0 +1,89 @@
+<template>
+	<div>
+		<router-link
+				to="/"
+				class="header-abs"
+		>
+			<div class="iconfont header-abs-back">&#xe624;</div>
+		</router-link>
+		<div
+				class="header-fixed"
+				:style="opacityStyle"
+				v-show="isShowHeader"
+		>
+			<router-link to="/">
+				<div class="iconfont header-fixed-back">&#xe624;</div>
+			</router-link>
+			景点详情
+		</div>
+	</div>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+		data() {
+     	return {
+        opacityStyle: {
+          opacity: 1
+        }
+      }
+		},
+		computed: {
+      isShowHeader() {
+      	  return this.opacityStyle.opacity
+			}
+		},
+		methods: {
+      handleScroll(e) {
+        const scrollOffset =  (1 - (document.documentElement.scrollTop) / 180);
+				let opacity = scrollOffset < 0 ? 0 : scrollOffset;
+				this.opacityStyle = { opacity }
+				console.log(1)
+			}
+		},
+		activated () {
+      window.addEventListener('scroll', this.handleScroll);
+		},
+		deactivated () {
+      window.removeEventListener('scroll', this.handleScroll)
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+	@import '~styles/varibles.styl'
+	.header-abs
+		position: absolute
+		left: .2rem
+		top: .2rem
+		width: .8rem
+		height: .8rem
+		line-height: .8rem
+		border-radius: .4rem
+		text-align: center
+		background: rgba(0, 0, 0, .8)
+		.header-abs-back
+			color: #fff
+			font-size: .4rem
+	.header-fixed
+		z-index: 2
+		position: fixed
+		top: 0
+		left: 0
+		right: 0
+		height: $headerHeight
+		line-height: $headerHeight
+		text-align: center
+		color: #fff
+		background: $bgColor
+		font-size: .32rem
+		.header-fixed-back
+			position: absolute
+			top: 0
+			left: 0
+			width: .64rem
+			text-align: center
+			font-size: .4rem
+			color: #fff
+</style>
